19953389e8b4344a30a60c2b14335e1b2b8cdcb0
Jean Roberto de Castro Zeferino <jean.zef13@gmail.com>
2023-03-20 20:13:08 -0300

    Ex 07

--- EcercicioM01/ex07/Program.cs
+++ EcercicioM01/ex07/Program.cs
@@ -11,19 +11,36 @@ namespace ex07
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("SORTEADOR DE NÚMERO");
-            Console.WriteLine("---------------------");
-
-            Console.Write($"Início: ");
-            int inicio = 0;
-            int.TryParse(Console.ReadLine(), out inicio);
+            int ini, fim;
+            Console.WriteLine("\nSORTEADOR DE NÚMERO");
+            Console.WriteLine("-----------------------");
+            Console.Write("Início: ");
+            int.TryParse(Console.ReadLine(), out ini);
             Console.Write("Fim: ");
-            int fim = 0;
-            int.TryParse(Console.ReadLine(),out fim);
-            Console.WriteLine("---------------------");
-            Console.WriteLine("Sorteando...");
-            Thread.Sleep(3000);
-            Console.Write("feito");
+            int.TryParse(Console.ReadLine(), out fim);
+
+
+
+            Console.WriteLine("-----------------------");
+            /* aqui o SetCursorPosition indica a posição que ele (ira estar) ou (aparecer) 
+            E lembrando que o SetCursorPosition é colocado no codigo em cima da referencia em que ele 
+            ira posicionar */
+            Console.SetCursorPosition(0, 6);
+            Console.Write("Sorteando...");
+            /* Aqui o Thread é o temporizador*/
+            Thread.Sleep(2000);
+
+
+
+            /* Aqui o Random é o gerador de numeros aleatorios */
+            Random gerador = new Random();
+            /* aqui o int num vai receber o gerador que ira gerar o proximo número que vai do 
+             inicio ao fim */
+            int num = gerador.Next(ini, fim + 1);
+            /*aqui o SetCursorPosition vai fazer com que apareça na tela o nesta posição 
+             assim irá substituir o Sorteando...  sobre pondo ele*/
+            Console.SetCursorPosition(0, 6);
+            Console.WriteLine($"Entre {ini} e {fim} sorteei o valor {num}");
 
 
             Console.ReadKey();
