0a23b0afee885c290afaf92b05bb517c9457cd66
Jean Roberto de Castro Zeferino <jean.zef13@gmail.com>
2023-03-20 17:31:50 -0300

    atualizando repositório

---
+++ EcercicioM01/ex06/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ex06
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            panResultado.Visibility = Visibility.Visible;
+        }
+
+        private void btnOk_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnOk_Click_2(object sender, RoutedEventArgs e)
+        {
+            int atual = DateTime.Now.Year;
+            int nasc;
+            int.TryParse(txtNasc.Text, out nasc);
+            int idade = atual - nasc;
+            lblMsg1.Content = $"Estamos no ano de {atual}.";
+            lblMsg2.Content = $"Quem nasceu em {nasc:D} vai ter {idade:D} anos.";
+
+            panResultado.Visibility = Visibility.Visible;
+        }
+    }
+}
---
+++ EcercicioM01/ex07/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ex07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SORTEADOR DE NÚMERO");
+            Console.WriteLine("---------------------");
+
+            Console.Write($"Início: ");
+            int inicio = 0;
+            int.TryParse(Console.ReadLine(), out inicio);
+            Console.Write("Fim: ");
+            int fim = 0;
+            int.TryParse(Console.ReadLine(),out fim);
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Sorteando...");
+            Thread.Sleep(3000);
+            Console.Write("feito");
+
+
+            Console.ReadKey();
+        }
+    }
+}
